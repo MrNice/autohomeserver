63a8765d04b697cef948a0e195ba83de950a5a86
Nicholas van de walle <nicholas@raspberrypi>
2015-03-29 22:56:54 +0000

    v0.0.1 server works

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 var WebSocketServer = require('ws').Server;
 var express = require('express');
+var cors = require('cors');
 
 var wss = new WebSocketServer({ port: 1337 });
 
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
 app.listen(80);
+app.use(cors());
+app.use(express.static(__dirname + '/public'));
 
-app.post('/on', function() {
-  ws.clients.forEach(function sendOn(client) {
-    client.send('turnOff'); // Literally the method name bc javascript 
+app.post('/on', function(req, res, next) {
+  wss.clients.forEach(function sendOn(client) {
+    client.send('turnOn'); // Literally the method name bc javascript 
   });
+
+  res.status(200).json({ status: 'OK' });
+  next();
 });
 
-app.post('/off', function() {
-  ws.clients.forEach(function sendOn(client) {
+app.post('/off', function(req, res, next) {
+  wss.clients.forEach(function sendOn(client) {
     client.send('turnOff'); // Literally the method name bc javascript 
   });
+  
+  res.status(200).json({ status: 'OK' });
+  next();
 });
